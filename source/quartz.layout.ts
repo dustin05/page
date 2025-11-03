8c8fa430ef4e6acfb509acbd66e7201cff9465ee
dustin05 <dustin.mcdaniel@gmail.com>
2025-11-03 17:36:36 -0500

    Refactor defaultContentPageLayout structure
    Refactored defaultContentPageLayout to simplify the left section and remove unnecessary components.

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -15,26 +15,6 @@ export const sharedPageComponents: SharedLayout = {
 }
 
 // components for pages that display a single page (e.g. a single note)
-//export const defaultContentPageLayout: PageLayout = {
-//  beforeBody: [
-//    Component.Breadcrumbs(),
-//    Component.ArticleTitle(),
-//    Component.ContentMeta(),
-//    Component.TagList(),
-//  ],
-//  left: [
-//    Component.PageTitle(),
-//    Component.Search(),
-//    Component.MobileOnly(Component.Spacer()),
-//    Component.Darkmode(),
-//    Component.DesktopOnly(Component.Explorer()),
-//  ],
-//  right: [
-//    Component.Graph(),
-//    Component.DesktopOnly(Component.TableOfContents()),
-//    Component.Backlinks(),
-//  ],
-//}
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
@@ -44,18 +24,10 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   left: [
     Component.PageTitle(),
+    Component.Search(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Flex({
-      components: [
-        {
-          Component: Component.Search(),
-          grow: true,
-        },
-        { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() },
-      ],
-    }),
-    Component.Explorer(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
     Component.Graph(),
