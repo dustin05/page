8a8c6eeb79da722fb3e8b050326389da7ed83ad1
dustin05 <dustin.mcdaniel@gmail.com>
2025-11-03 17:31:57 -0500

    Refactor defaultContentPageLayout structure
    Refactor defaultContentPageLayout to use ConditionalRender and Flex components for better layout control.

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -15,19 +15,50 @@ export const sharedPageComponents: SharedLayout = {
 }
 
 // components for pages that display a single page (e.g. a single note)
+//export const defaultContentPageLayout: PageLayout = {
+//  beforeBody: [
+//    Component.Breadcrumbs(),
+//    Component.ArticleTitle(),
+//    Component.ContentMeta(),
+//    Component.TagList(),
+//  ],
+//  left: [
+//    Component.PageTitle(),
+//    Component.Search(),
+//    Component.MobileOnly(Component.Spacer()),
+//    Component.Darkmode(),
+//    Component.DesktopOnly(Component.Explorer()),
+//  ],
+//  right: [
+//    Component.Graph(),
+//    Component.DesktopOnly(Component.TableOfContents()),
+//    Component.Backlinks(),
+//  ],
+//}
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+    Component.ConditionalRender({
+      component: Component.Breadcrumbs(),
+      condition: (page) => page.fileData.slug !== "index",
+    }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
     Component.PageTitle(),
-    Component.Search(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+        { Component: Component.ReaderMode() },
+      ],
+    }),
+    Component.Explorer(),
   ],
   right: [
     Component.Graph(),
