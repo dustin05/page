908b341c241a1e4d447c1858cd05d0acfcd7233b
dustin05 <dustin.mcdaniel@gmail.com>
2025-11-03 17:20:09 -0500

    Simplify beforeBody layout definition
    Refactor beforeBody layout to a single line.

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -16,12 +16,13 @@ export const sharedPageComponents: SharedLayout = {
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
-  beforeBody: [
-    Component.Breadcrumbs(),
-    Component.ArticleTitle(),
-    Component.ContentMeta(),
-    Component.TagList(),
-  ],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+//  beforeBody: [
+//    Component.Breadcrumbs(),
+//    Component.ArticleTitle(),
+//    Component.ContentMeta(),
+//    Component.TagList(),
+//  ],
   left: [
     Component.PageTitle(),
     Component.Search(),
