c8aa911e4a76e76d5a761b7db9aee6063b532bbe
dustin05 <dustin.mcdaniel@gmail.com>
2025-11-03 16:05:33 -0500

    Update quartz.layout.ts

--- source/quartz.layout.ts
+++ source/quartz.layout.ts
@@ -25,7 +25,6 @@ export const defaultContentPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
   ],
